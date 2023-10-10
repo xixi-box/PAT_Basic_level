a8247658a4fdac4dbfbb32f3257b4d352599a01d
ws <2606209307@qq.com>
2023-10-10 21:12:40 +0800

    fix 部分A+B这道题牛客能过，pat上限制时间为150ms，使用stringBuilder改了一遍,可以通过

--- niuke06.java
+++ niuke06.java
@@ -3,30 +3,27 @@ import java.util.Scanner;
 public class niuke06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String a = scanner.next();
+        char[] a = scanner.next().toCharArray();
         String da = scanner.next();
-        String b = scanner.next();
+        char[] b = scanner.next().toCharArray();
         String db = scanner.next();
-        String[] arra = a.split("");
-        String[] arrb = b.split("");
-        String num1 = "";
-        for (int i = 0; i < arra.length; i++) {
-            if (arra[i].equals(da)) {
-                num1 += da;
+        StringBuilder num1 = new StringBuilder();
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == da.charAt(0)) {
+                num1.append(da);
             }
         }
-        String num2 = "";
-        for (int i = 0; i < arrb.length; i++) {
-            if (arrb[i].equals(db)) {
-                num2 += db;
+        StringBuilder num2 = new StringBuilder();
+        for (int i = 0; i < b.length; i++) {
+            if (b[i] == db.charAt(0)) {
+                num2.append(db);
             }
         }
-        if (num1.equals("") || num2.equals("")) {
+        if (num1.toString().equals("") || num2.toString().equals("")) {
             System.out.println("0");
         } else {
-            System.out.println(Integer.parseInt(num1) + Integer.parseInt(num2));
+            System.out.println(Integer.parseInt(num1.toString()) + Integer.parseInt(num2.toString()));
         }
-
 
     }
 }
